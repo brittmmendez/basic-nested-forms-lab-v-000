a36b294dcd8814da7200673496222fc52a13907d
Brittany Morris <brittmmendez@gmail.com>
2018-02-19 01:25:09 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -14,7 +14,8 @@ class RecipesController < ApplicationController
   end
 
   def create
-    Recipe.create(recipe_params)
+    recipe=Recipe.create(recipe_params)
+    redirect_to recipe
   end
 
   private
